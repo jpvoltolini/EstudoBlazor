d457de5c299c83066994cfe48e9dd346e9ff6e2e
João Pedro Voltolini <jptheshogun@gmail.com>
2024-09-05 16:55:31 -0300

    Primeiro Commit

---
+++ Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorWebAssembCurso.Models
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+
+        public int Quantidade { get; set; }
+
+        public double Preco { get; set; }
+    
+    
+        public double ValorEstoqueTotal()
+        {
+            return Preco*Quantidade; 
+        }
+
+
+    }
+}
