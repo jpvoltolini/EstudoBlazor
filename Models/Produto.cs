e26d6c06e56ce5e669feb38cdda0eec2132787e9
João Pedro Voltolini <jptheshogun@gmail.com>
2024-09-05 19:00:18 -0300

    Atualização aula 20

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,11 +1,18 @@
-﻿namespace BlazorWebAssembCurso.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWebAssembCurso.Models
 {
     public class Produto
     {
+        [Required(ErrorMessage ="Campo Obrigatório")]
         public string Nome { get; set; }
 
+        [Required]
+        [Range(1,100, ErrorMessage ="Apenas até 100 unidades") ]
         public int Quantidade { get; set; }
-
+        
+        [Required]
+        [Range(0.1, 100, ErrorMessage = "Apenas até 100 pila")]
         public double Preco { get; set; }
     
     
